4d1dcd6100ee4c3cb9ebf59b18031a0e6717c6bb
JonasEwn <jonas.esswein@gmail.com>
2023-10-17 16:17:42 +0200

    Initial commit

---
+++ src/main/java/com/example/teamsieben/TeamSiebenApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamsieben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSiebenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSiebenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamsieben/TeamSiebenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamsieben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSiebenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
