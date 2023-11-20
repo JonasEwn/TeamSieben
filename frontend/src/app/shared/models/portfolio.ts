f5858750ef077c1dc3ba8d9b244ac6dc0ba08a25
Dennis Do <do.dennis.99@gmail.com>
2023-11-20 18:58:16 +0100

    Verbesserung der Sprint02 Aufgabe 1

--- frontend/src/app/shared/models/portfolio.ts
+++ frontend/src/app/shared/models/portfolio.ts
@@ -3,15 +3,79 @@ export interface Portfolio {
   id: number;
   wkn: string;
   name: string;
-  price: string;
+  price: number;
   quantity: number;
   description: string;
   category: string;
   purchaseDate: string;
   totalPrice: number;
-  
-  
-
-
-
 }
+  export const portfolio = [
+  {
+    id: 1,
+    name: 'Allianz',
+    price: 100,
+    wkn: '123456',
+    quantity: 100,
+    description: 'Versicherungsgesellschaft',
+    category: 'Aktie',
+    purchaseDate: '01.11.2023',
+    totalPrice: 1
+  },
+  {
+    id: 2,
+    name: 'Allianz',
+    price: 200,
+    wkn: '123456',
+    quantity: 200,
+    description: 'Versicherungsgesellschaft',
+    category: 'Aktie',
+    purchaseDate: '02.11.2023',
+    totalPrice: 1
+  },
+  {
+    id: 3,
+    name: 'BASF',
+    price: 50,
+    wkn: '987654',
+    quantity: 50,
+    description: 'Chemie Unternehmen',
+    category: 'Aktie',
+    purchaseDate: '02.11.2023',
+    totalPrice: 1
+  },
+  {
+    id: 4,
+    name: 'BASF',
+    price: 40,
+    wkn: '987654',
+    quantity: 100,
+    description: 'Chemie Unternehmen',
+    category: 'Aktie',
+    purchaseDate: '02.11.2023',
+    totalPrice: 1
+  },
+  {
+    id: 5,
+    name: 'BASF',
+    price: 30,
+    wkn: '987654',
+    quantity: 200,
+    description: 'Chemie Unternehmen',
+    category: 'Aktie',
+    purchaseDate: '03.11.2023',
+    totalPrice: 1
+  },
+  {
+    id: 6,
+    name: 'Bitcoin',
+    price: 32000,
+    wkn: 'BTC',
+    quantity: 1,
+    description: 'Kryptowährung',
+    category: 'Crypto',
+    purchaseDate: '03.11.2023',
+    totalPrice: 1
+  },
+  
+  ];
--- frontend/src/app/shared/services/http/portfolio.service.ts
+++ frontend/src/app/shared/services/http/portfolio.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Portfolio } from '../../models/portfolio';
+import { Portfolio, portfolio } from '../../models/portfolio';
 import { Observable } from 'rxjs';
 
 // Deklariert den PortfolioService als Injectable-Service
@@ -22,81 +22,14 @@ export class PortfolioService {
 
   public addPortfolio(): Observable<Portfolio> {
     return new Observable<Portfolio>(() => {
-      // Erstellt ein neues Observable, das den Klick auf den Button "Add Portfolio" auslöst
+       //Erstellt ein neues Observable, das den Klick auf den Button "Add Portfolio" auslöst
       console.log('addPortfolio clicked');
     });
   }
 
 
   private generatePortfolioList(): void { // Erzeugt eine Beispiel-Portfolioliste mit mehreren Portfolios
-    this.portfolioList = [
-      {
-        id: 1,
-        name: 'Allianz',
-        price: '100',
-        wkn: '123456',
-        quantity: 100,
-        description: 'Versicherungsgesellschaft',
-        category: 'Aktie',
-        purchaseDate: '01.11.2023',
-        totalPrice: 1
-      },
-      {
-        id: 2,
-        name: 'Allianz',
-        price: '200',
-        wkn: '123456',
-        quantity: 200,
-        description: 'Versicherungsgesellschaft',
-        category: 'Aktie',
-        purchaseDate: '02.11.2023',
-        totalPrice: 1
-      },
-      {
-        id: 3,
-        name: 'BASF',
-        price: '50',
-        wkn: '987654',
-        quantity: 50,
-        description: 'Chemie Unternehmen',
-        category: 'Aktie',
-        purchaseDate: '02.11.2023',
-        totalPrice: 1
-      },
-      {
-        id: 4,
-        name: 'BASF',
-        price: '40',
-        wkn: '987654',
-        quantity: 100,
-        description: 'Chemie Unternehmen',
-        category: 'Aktie',
-        purchaseDate: '02.11.2023',
-        totalPrice: 1
-      },
-      {
-        id: 5,
-        name: 'BASF',
-        price: '30',
-        wkn: '987654',
-        quantity: 200,
-        description: 'Chemie Unternehmen',
-        category: 'Aktie',
-        purchaseDate: '03.11.2023',
-        totalPrice: 1
-      },
-      {
-        id: 6,
-        name: 'Bitcoin',
-        price: '32000',
-        wkn: 'BTC',
-        quantity: 1,
-        description: 'Kryptowährung',
-        category: 'Crypto',
-        purchaseDate: '03.11.2023',
-        totalPrice: 1
-      },
-    ]; 
+    this.portfolioList = [...portfolio]; 
     
     // Gruppiert die Portfolios nach WKN und berechnet den Durchschnittspreis und den Gesamtpreis für jede WKN
 const portfolioMap = new Map<string, Portfolio[]>();
@@ -110,13 +43,13 @@ for (const portfolio of this.portfolioList) {
 }
 
 for (const [wkn, portfolios] of portfolioMap) {
-  const totalPrice = portfolios.reduce((acc, portfolio) => acc + parseFloat(portfolio.price) * portfolio.quantity, 0);
+  const totalPrice = portfolios.reduce((acc, portfolio) => acc + portfolio.price * portfolio.quantity, 0);
   const roundedTotalPrice = parseFloat(totalPrice.toFixed(2));
   const totalQuantity = portfolios.reduce((acc, portfolio) => acc + Number(portfolio.quantity), 0);
   const averagePrice = totalPrice / totalQuantity;
 
   for (const portfolio of portfolios) {
-    portfolio.price = averagePrice.toFixed(2);
+    portfolio.price = parseFloat(averagePrice.toFixed(2));
     portfolio.totalPrice = roundedTotalPrice;
   }
 }
