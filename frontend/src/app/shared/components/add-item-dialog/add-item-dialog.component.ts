b186ffa7946db31486f48fd22b7723a4949385bf
FurkanVu <vural.f00@gmail.com>
2023-11-21 16:48:34 +0100

    Anzahl, Kategorie und Beschreibung hinzugefügt

--- frontend/src/app/shared/components/add-item-dialog/add-item-dialog.component.ts
+++ frontend/src/app/shared/components/add-item-dialog/add-item-dialog.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+
+
 
 // This component is used to add a new item to the list.
 // It is used in the overview.component.html.
@@ -26,11 +28,18 @@ import { MatInputModule } from '@angular/material/input';
 export class AddItemDialogComponent {
   public myForm: FormGroup;
 
+
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
       name: ['', Validators.required],
       price: ['', Validators.required],
       wkn: ['', [Validators.required]],
+      quantity: ['', Validators.required],
+      category: ['', Validators.required],
+      description: ['', Validators.required]
     });
   }
+
+
+  
 }
